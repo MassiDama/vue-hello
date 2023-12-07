2bc904a7a7585aadfb12954e21d5a99a7289b7f1
Massidama <massidama@outlook.it>
2023-12-07 16:41:23 +0100

    feat: added image

--- js/scritp.js
+++ js/scritp.js
@@ -4,7 +4,8 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            messaggio: "Hello Vue!"
+            messaggio: "Hello Vue!",
+            foto: " https://res.cloudinary.com/practicaldev/image/fetch/s--q6AIFazN--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/ll22z8mea106b4vjdjy7.jpeg "
         }
     },
 
